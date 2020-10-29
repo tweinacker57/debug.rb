c9215d0cde95e1df77317dd60428e897cd243feb
Tyler Weinacker <tweinacker57@gmail.com>
2020-10-28 21:03:15 -0400

    Another exercise in a debugging exercise

--- debug.rb
+++ debug.rb
@@ -1,7 +1,26 @@
-puts "Hello! Please enter a greeting: "
-greeting = gets.chomp
-if greeting == "Arrr!"
-  puts "Go away, pirate."
-else
-  puts "Greetings, hater of pirates!"
+# puts "Hello! Please enter a greeting: "
+# greeting = gets.chomp
+# if greeting == "Arrr!"
+#   puts "Go away, pirate."
+# else
+#   puts "Greetings, hater of pirates!"
+# end
+
+class Person
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  def first_name
+    @first_name
+  end
+  def to_s
+    @first_name + " " + @last_name
+  end
 end
+person1 = Person.new("Thomas", "Jefferson")
+puts person1.first_name
+puts person1
+
+person2 = Person.new("Teddy", "Roosevelt")
+puts person2
